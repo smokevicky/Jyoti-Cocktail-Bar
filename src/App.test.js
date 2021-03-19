dd2eed78eb32050082beeada0f2172f310c22fa0
Jyoti Prakash Jena <jjena@trugreenmail.com>
2021-03-19 14:59:58 +0530

    Updated testbed file

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,8 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+// Unfortunately, I don't have any testing experience.
+// I know the Assignment description says to have a testbed ready
+// But I don't want to copy a code from internet that I don't understand
+// I am keen on learning it, if given a chance but I don't have any experience as of now, unfortunately
